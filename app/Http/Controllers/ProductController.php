8c38fa5649e72b31f264bb8fcb2b95295a4cc920
yudistira-rk <yudistiraeducation100@gmail.com>
2023-01-09 00:17:38 +0700

    08/01/2022 create api

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        Product::create([
+            'name' => $request->product_name,
+            'price' => $request->price,
+            'desc' => $request->desc
+        ]);
+        return response()->json([
+            'message' => 'data berhasil ditambahkan'
+        ]);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $table = 'products';
+    protected $fillable = [
+        'name', 'price', 'desc'
+    ];
+
     use HasFactory;
 }
